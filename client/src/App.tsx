dd38cf0f961b91206634f55399d4330302382729
ayoolasam <obayomisamuel941@gmail.com>
2024-06-12 14:36:33 +0100

    commit

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+
+
+
+
+function App() {
+
+
+  return (
+    <div className="h-screen flex justify-center items-center bg-gray-700">
+ <h1 className="text-white text-3xl font-bold">HomePage</h1>
+    </div>
+   
+  )
+}
+
+export default App
